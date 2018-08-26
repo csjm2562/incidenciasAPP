2b3296d6872fa215197d43bb6f1c28c2ef27c671
Johan Solano <johanmsolanoc@gmail.com>
2018-08-26 10:25:10 -0400

    reasignar incidencia
    -actualizacion de la interfaz.
    -se añadió funcion agregar_RI para guardar los datos en la base de datos por medio de AJAX.

---
+++ funciones/inc/reasignarIncidencia/guardarRI.php
@@ -0,0 +1,25 @@
+<?php
+  $mysqli = new mysqli("localhost","johan","root","login"); //MODIFICAR
+  if ($mysqli->connect_errno) {
+    echo "Falló al conectar".$mysqli->connect_errno;
+  } else {
+    if (empty($_POST['incidencia'])) {
+      echo "Por favor ingrese el ID de la incidencia.";
+      exit();
+    }
+    if (empty($_POST['usuario'])) {
+      echo "Por favor ingrese un empleado para asignarle la incidencia ".$incidencia = $_POST["incidencia"];
+      exit();
+    }
+    if (!empty($_POST['incidencia']) && !empty($_POST['usuario'])) {
+      $incidencia = $_POST["incidencia"];
+      $idUsuario = $_POST["usuario"];
+      $query = "UPDATE incidencia SET id_empleado='$idUsuario' WHERE idIncidencia='$incidencia'";
+      $resultado = $mysqli->query($query);
+      echo "La incidencia ".$incidencia." fué asignada correctamente.";
+    } else {
+      echo "Error desconocido.";
+    }
+  }
+  exit();
+?>
--- funciones/inc/reasignarIncidencia/reasignarIncidencia.php
+++ funciones/inc/reasignarIncidencia/reasignarIncidencia.php
@@ -1,17 +1,18 @@
 <form role="form" id="agregar_RI" name="agregar_RI" method="post">
-  <div class="input-field">
-    <input class="validate" type="text" name="incidencia">
-    <label for="incidencia">Ingrese ID de la incidencia</label>
-  </div><br>
-  <div class="input-field col s5">
-    <select name="usuario" id="usuario">
-      <option value="" disabled selected>Selecciona una opción</option>
-    </select>
-    <label>Desplegable empleados</label>
-  </div><br>
+  <div class="row">
+    <div class="input-field col s6">
+      <input class="validate" type="text" name="incidencia">
+      <label for="incidencia">Ingrese ID de la incidencia</label>
+    </div>
+    <div class="input-field col s6">
+      <select name="usuario" id="usuario">
+        <option value="" disabled selected>Empleados</option>
+      </select>
+    </div>
+  </div>
   <center>
     <div class="row">
-      <div id="resultados_ri"></div>
+      <div id="resultados_ri"></div><br>
       <button type="submit" class="col s12 btn btn-large waves-effect">Enviar</button>
     </div>
   </center>
@@ -28,5 +29,17 @@
     error:function(){
       $select.html('<option id="-1">Error!</option>');
     }
+  });
+  $('#agregar_RI').submit(function(event){
+    var parametros = $(this).serialize();
+    $.ajax({
+      type: "POST",
+      url: "funciones/inc/reasignarIncidencia/guardarRI.php",
+      data: parametros,
+      success: function(data) {
+        $('#resultados_ri').html(data);
+      }
+    });
+    event.preventDefault();
   });
 </script>
