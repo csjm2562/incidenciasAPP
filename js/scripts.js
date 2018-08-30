8089a97dbb0085867eaa0ccf5dacac27d3cc373c
Johan Solano <johanmsolanoc@gmail.com>
2018-08-30 11:56:34 -0400

    scripts
    -Se actualizaron las rutas.

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,98 @@
 $(document).ready(function() {
-  $('.altEmpCli').on('click', function() {
-      $('.navega li').removeClass('active');
-      $("#contenido").load('funciones/inc/altaEmpleadoCliente.php');
-        return false;
-  });
-  $('.altIncPetAdmCli').on('click', function() {
-      $('.navega li').removeClass('active');
-      $("#contenido").load('funciones/inc/altaIncidenciaPeticionAdministradorCliente/altaIncidenciaPeticionAdministradorCliente.php');
-        return false;
+  $('.altIncAdm').on('click', function() {
+    $('.navega li').removeClass('active');
+    $("#contenido").load('funciones/inc/altaIncidenciaAdministrador.php');
+      return false;
   });
   $('.altIncEmp').on('click', function() {
-      $('.navega li').removeClass('active');
-      $("#contenido").load('funciones/inc/altaIncidenciaEmpleado.php');
-        return false;
+    $('.navega li').removeClass('active');
+    $("#contenido").load('funciones/inc/altaIncidenciaEmpleado.php');
+      return false;
+  });
+  $('.altPetAdm').on('click', function() {
+    $('.navega li').removeClass('active');
+    $("#contenido").load('funciones/inc/altaPeticionAdministrador.php');
+      return false;
+  });
+  $('.altPetEmp').on('click', function() {
+    $('.navega li').removeClass('active');
+    $("#contenido").load('funciones/inc/altaPeticionAdministrador.php');
+      return false;
   });
   $('.asiInc').on('click', function() {
-      $('.navega li').removeClass('active');
-      $("#contenido").load('funciones/inc/asignarmeIncidencia/asignarmeIncidencia.php');
-        return false;
+    $('.navega li').removeClass('active');
+    $("#contenido").load('funciones/inc/asignarmeIncidencia.php');
+      return false;
   });
   $('.bajEmpCli').on('click', function() {
     $('.navega li').removeClass('active');
     $("#contenido").load('funciones/inc/bajaEmpleadoCliente.php');
       return false;
   });
-  $('.conBajPro').on('click', function() {
-    $('.navega li').removeClass('active');
-    $("#contenido").load('funciones/inc/contratarBajarProducto/contratarBajarProducto.php');
-      return false;
-  });
   $('.detInc').on('click', function() {
     $('.navega li').removeClass('active');
-    $("#contenido").load('funciones/inc/detalleIncidencia/detalleIncidencia.php');
+    $("#contenido").load('funciones/inc/detalleIncidencia.php');
       return false;
+  });
+  $('.incAsiAMi').on('click', function() {
+    $('.navega li').removeClass('active');
+    $('#contenido').load('funciones/inc/incidenciasAsignadasAMi.php');
   });
   $('.incSinAsi').on('click', function() {
     $('.navega li').removeClass('active');
-    $("#contenido").load('funciones/inc/incidenciasSinAsignar/incidenciasSinAsignar.php');
+    $("#contenido").load('funciones/inc/incidenciasSinAsignar.php');
       return false;
   });
-  $('.misInc').on('click', function() {
+  $('.misIncEmp').on('click', function() {
     $('.navega li').removeClass('active');
-    $("#contenido").load('funciones/inc/misIncidencias/misIncidencias.php');
+    $("#contenido").load('funciones/inc/misIncidenciasEmpleado.php');
       return false;
   });
-  $('.modInc').on('click', function() {
+  $('.misIncCli').on('click', function() {
     $('.navega li').removeClass('active');
-    $("#contenido").load('funciones/inc/modificarIncidencia/modificarIncidencia.php');
+    $("#contenido").load('funciones/inc/misIncidenciasCliente.php');
       return false;
   });
   $('.modEmpCli').on('click', function() {
     $('.navega li').removeClass('active');
-    $("#contenido").load('funciones/inc/modificarEmpleadoCliente/modificarEmpleadoCliente.php');
+    $("#contenido").load('funciones/inc/modificarEmpleadoCliente.php');
+      return false;
+  });
+  $('.modInc').on('click', function() {
+    $('.navega li').removeClass('active');
+    $("#contenido").load('funciones/inc/modificarIncidencia.php');
       return false;
   });
   $('.reabrirInc').on('click', function() {
     $('.navega li').removeClass('active');
-    $("#contenido").load('funciones/inc/reabrirIncidencia/reabrirIncidencia.php');
+    $("#contenido").load('funciones/inc/reabrirIncidencia.php');
       return false;
   });
   $('.reasignarInc').on('click', function() {
     $('.navega li').removeClass('active');
-    $("#contenido").load('funciones/inc/reasignarIncidencia/reasignarIncidencia.php');
+    $("#contenido").load('funciones/inc/reasignarIncidencia.php');
       return false;
   });
   $('.reportes').on('click', function() {
     $('.navega li').removeClass('active');
-    $("#contenido").load('funciones/inc/reporte/reportes.php');
+    $("#contenido").load('funciones/inc/reportes.php');
+      return false;
+  });
+
+
+  $('.altEmpCli').on('click', function() {
+      $('.navega li').removeClass('active');
+      $("#contenido").load('funciones/inc/altaEmpleadoCliente/altaEmpleadoCliente.php');
+        return false;
+  });
+  $('.altIncPetAdmCli').on('click', function() {
+      $('.navega li').removeClass('active');
+      $("#contenido").load('funciones/inc/altaIncidenciaAdministrador.php');
+        return false;
+  });
+  $('.conBajPro').on('click', function() {
+    $('.navega li').removeClass('active');
+    $("#contenido").load('funciones/inc/contratarBajarProducto/contratarBajarProducto.php');
       return false;
   });
 });
