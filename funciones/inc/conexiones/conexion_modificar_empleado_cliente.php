32d3a913b107400ecd5ecc049e787ffce6bb6a6b
Johan Solano <johanmsolanoc@gmail.com>
2018-09-01 01:46:04 -0400

    modifica empleado
    -Conexion a la base de datos que muestra todos los productos de la base de datos.

---
+++ funciones/inc/conexiones/conexion_modificar_empleado_cliente.php
@@ -0,0 +1,36 @@
+<?php
+  $mysqli = new mysqli("localhost","johan","root","incidenciasapp"); //MODIFICAR
+  if ($mysqli->connect_errno) {
+    echo "Falló al conectar".$mysqli->connect_errno;
+  } else {
+    if (empty($_POST['nombreU'])) {
+      echo "Por favor ingrese el nombre de usuario.";
+      exit();
+    }
+    if (!empty($_POST['nombreU'])) {
+      echo "<h4>Productos</h4>";
+      $query = "SELECT * FROM producto";
+      $contador = 0;
+      $resultado = $mysqli->query($query);
+      while($row = mysqli_fetch_array($resultado)) {
+        if($contador == 0 ) {
+          echo "<div class='col s3'>";
+          $contador++;
+        } else {
+          $contador++;
+        }
+          echo "<p><label>
+                  <input type='checkbox' name='productos[]' value='".$row["id_producto"]."'>
+                  <span>".$row['descripcion']."</span>
+                </label></p>";
+          if ($contador == 10) {
+            echo "</div>";
+            $contador = 0;
+          }
+      }
+    } else {
+      echo "Error desconocido.";
+    }
+  }
+  exit();
+?>
