070d368ade01c091499fef5e9255783b8da5bc3b
Johan Solano <johanmsolanoc@gmail.com>
2018-09-08 00:46:31 -0400

    mis incidencias, modificar incidencia., modificar empleado/cliente
    -los campos se validan.
    -las tablas ahoras son responsivas.

--- funciones/inc/modificarIncidencia.php
+++ funciones/inc/modificarIncidencia.php
@@ -1,11 +1,14 @@
+<h4>Modificar incidencia</h4><br>
 <form role="form" name="modificar" id="modificar" method="post">
+  <input value="<?php session_start(); echo ''.$_SESSION['id_usuario']; ?>" type="hidden" name="usuario">
   <div class="row">
     <div class="input-field col s6">
-      <input class="validate" type="text" name="incidencia">
+      <input class="validate" type="text" name="incidencia" required>
       <label for="incidencia">Ingrese ID de la incidencia</label>
+      <span class="helper-text" data-error="Este campo no puede estar vacío."></span>
     </div>
     <div class="input-field col s12">
-      <textarea class="materialize-textarea" name="comentario"></textarea>
+      <textarea class="materialize-textarea" name="comentario" required></textarea>
       <label for="comentario">Comentarios</label>
     </div>
     <button type="submit" class="btn btn-large waves-effect right">Guardar</button>
