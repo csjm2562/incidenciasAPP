15d0879c5e25a9bba8469e0c58cbc19d1f056d42
Johan Solano <johanmsolanoc@gmail.com>
2018-09-08 00:51:11 -0400

    alta incidencia empleado.
    -el campo 'localizacion' ahora es un 'select' auto relleno con valores del 1 al 20.
    -se validan los datos.
    -correccion de traduccion.

--- funciones/inc/altaIncidenciaEmpleado.php
+++ funciones/inc/altaIncidenciaEmpleado.php
@@ -4,8 +4,14 @@ echo "".strtotime("selec_hasta");
 <form role="form" id="alta_incidencia_empleado" name="alta_incidencia_empleado" method="post">
   <div class="row">
     <div class="input-field col s6">
-      <input id="localizacion" class="validate" type="text" name="localizacion" pattern="[0-9]+" data-length="20">
-      <label for="localizacion">Seleccione localización</label>
+      <select name="localizacion" id="localizacion">
+        <option value="" disabled selected>Localización</option>
+        <?php
+          for ($i=1; $i <= 20; $i++) {
+            echo "<option value='$i'>$i</option>";
+          }
+        ?>
+      </select>
     </div>
     <div class="input-field col s6">
       <select name="cliente" id="cliente">
@@ -28,7 +34,7 @@ echo "".strtotime("selec_hasta");
     </div>
   </div>
   <div class="input-field col s12">
-    <textarea class="materialize-textarea" name="comentario"></textarea>
+    <textarea class="materialize-textarea" name="comentario" required></textarea>
     <label for="comentario">Comentarios</label>
   </div><br>
   <center>
@@ -46,7 +52,7 @@ echo "".strtotime("selec_hasta");
       format: 'dd-mm-yyyy',
       i18n: {
         months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
-        monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Set", "Oct", "Nov", "Dic"],
+        monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
         weekdays: ["Domingo","Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
         weekdaysShort: ["Dom","Lun", "Mar", "Mie", "Jue", "Vie", "Sab"],
         weekdaysAbbrev: ["D","L", "M", "M", "J", "V", "S"],
